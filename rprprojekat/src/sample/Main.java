4520fdbb69e84de825e957defffe271c88031a88
jhasanovic <jhasanovic1@etf.unsa.ba>
2021-08-03 19:52:22 +0200

    Korisnički interfejs v1

---
+++ rprprojekat/src/sample/Main.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.util.ResourceBundle;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+
+
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/frontpage.fxml"));
+        Parent root = loader.load();
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
